7492750ce6e5ce273733f2f0f311f99336e66383
Diana Jiang <jiang99diana@gmail.com>
2021-11-02 11:52:33 -0400

    commit tree 4787d22c905d10df7e19fc40e476a54451fa6b36

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    let blocks = Math.abs(pickup - 42);
+    return blocks;
+}
+
+function distanceFromHqInFeet(pickup) {
+    let feet = distanceFromHqInBlocks(pickup) * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let distance = Math.abs((start - end) * 264);
+    return distance;
+}
+
+function calculatesFarePrice(start, destination) {
+    let fare;
+    let distance = Math.abs((start - destination) * 264);
+    if (distance < 400) {
+        return 0;
+    } else if (distance >= 400 && distance <= 2000) {
+        fare = (distance - 400) * 0.02;
+        return fare;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else if (distance > 2500) {
+        return `cannot travel that far`;
+    }
+}
